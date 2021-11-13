210fa0e9d5c8fcd4fbb4abc617c69bf1b73f846f
dzcxs6c <radek.bednarik@tesena.com>
2021-11-13 07:53:35 +0100

    _

--- scripts/01.spec.js
+++ scripts/01.spec.js
@@ -1,29 +1,34 @@
 import "chromedriver";
 import { Builder, By, until } from "selenium-webdriver";
 
-// find & click
+// iframe
 
 (async () => {
   const driver = await new Builder().forBrowser("chrome").build();
-  await driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+  await driver.get("https://the-internet.herokuapp.com/iframe");
 
   try {
-    await driver
-      .wait(
-        until.elementIsEnabled(
-          await driver.findElement(By.css("div#content button"))
-        ),
-        10000
-      )
-      .click();
-
-    await driver.wait(
-      until.elementIsEnabled(driver.findElement(By.css("div#elements button")))
+    const iframe = await driver.wait(
+      until.elementLocated(By.xpath("//iframe[@id='mce_0_ifr']"))
     );
+    await driver.switchTo().frame(iframe);
+    const p = await driver.wait(until.elementLocated(By.xpath("//body//p")));
+    const pText = await p.getText();
 
-    await driver.close();
+    console.log("Default text: ", pText);
+
+    const changedText = await driver.executeScript(() => {
+      const body_ = document.getElementById("tinymce");
+      const p_ = body_.getElementsByTagName("p");
+      p_.innerText = "Hello";
+      return p_.innerText;
+    });
+
+    console.log("Changed Text: ", changedText);
+
+    await driver.quit();
   } catch (error) {
     console.error(error);
-    await driver.close();
+    await driver.quit();
   }
 })();
